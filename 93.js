c864c60cb1ebee8255ef7de1be0994f81eddb329
xiegui511 <50509841+xiegui511@users.noreply.github.com>
2025-06-26 19:31:04 +0800

    Update 93.js

--- 93.js
+++ 93.js
@@ -1,6 +1,8 @@
 {
-"name": "91porn",
-"type": 3,
-"api": "csp_91porn",
-"ext": "https://raw.githubusercontent.com/xiegui511/clash_icons/main/91.js"
+  "name": "91Porn",
+  "api": "https://raw.githubusercontent.com/xiegui511/clash_icons/main/91.js",
+  "webSite": "https://91porn.com",
+  "remark": "91Porn 扩展，基于 JS 脚本解析",
+  "version": "1",
+  "type": 3
 }
